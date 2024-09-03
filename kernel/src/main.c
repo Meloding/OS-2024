6289df89f18fa612d5dd6b9016eab6e293490a3e
Huang Zirui <389608862@qq.com>
2024-09-03 15:44:00 +0800

    .

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -14,10 +14,10 @@ int main() {
   init_gdt();
   init_serial();
   init_fs();
-  //init_page(); // uncomment me at Lab1-4
-  //init_cte(); // uncomment me at Lab1-5
-  //init_timer(); // uncomment me at Lab1-7
-  init_proc(); // uncomment me at Lab2-1
+  //init_page(); // uncomment me at WEEK3-virtual-memory
+  //init_cte(); // uncomment me at WEEK2-interrupt
+  //init_timer(); // uncomment me at WEEK2-interrupt
+  init_proc(); // uncomment me at WEEK1-os-start
   //init_dev(); // uncomment me at Lab3-1
   printf("Hello from OS!\n");
   init_user_and_go();
@@ -25,13 +25,6 @@ int main() {
 }
 
 void init_user_and_go() {
-  // Lab1-2: ((void(*)())eip)();
-  // Lab1-4: pdgir, stack_switch_call
-  // Lab1-6: ctx, irq_iret
-  // Lab1-8: argv
-  // Lab2-1: proc
-  // Lab3-2: add cwd
-
   // WEEK1: ((void(*)())eip)();
   uint32_t eip = load_elf(NULL, "loaduser");
   proc_t* proc = proc_alloc();
